9c698f5cedc1a5a3225461cb0752d541f20b63bd
daydayfun <39835967+daydayfun@users.noreply.github.com>
2019-03-16 20:08:38 +0800

    Update main.c
    add comment

--- LinuxKernelDevTip/Example1/main.c
+++ LinuxKernelDevTip/Example1/main.c
@@ -1,6 +1,6 @@
-#include <linux/init.h>
-#include <linux/module.h>
-MODULE_LICENSE("GPL");
+#include <linux/init.h>     	//这个头文件包含了你的模块初始化与清除的函数
+#include <linux/module.h>  	 //这个头文件包含了许多符号与函数的定义，这些符号与函数多与加载模块有关
+MODULE_LICENSE("GPL");            // "GPL" 是指明了 这是GNU General Public License的任意版本
 
 static int __init init_modules(void)
 {
